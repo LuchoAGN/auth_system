07350bb8272fcfd34604d888821256b4e1399d6b
LuchoAGN <44987778+LuchoAGN@users.noreply.github.com>
2024-08-18 22:43:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,27 @@ from app.models import Base
 from app.database import engine
 from app.routers import user_router
 
-app = FastAPI()
+description = """
+Este proyecto es una API de autenticación y autorización construida con FastAPI y MySQL, utilizando JWT (JSON Web Tokens) 
+para la autenticación. Los usuarios pueden registrarse, iniciar sesión, y se les pueden asignar roles específicos como user 
+y admin. Este proyecto también incluye manejo de created_at y updated_at para rastrear la creación 
+y actualización de registros.
+"""
+
+app = FastAPI( title="Auth System",
+    description=description,
+    summary="Base Authentication & Authorization System",
+    version="0.0.1",
+    terms_of_service="https://github.com/LuchoAGN/auth_system?tab=MIT-1-ov-file",
+    contact={
+        "name": "Lucho AGN",
+        "url": "https://luchoagn.dev/",
+        "email": "contact@luchoagn.dev",
+    },
+    license_info={
+        "name": "MIT license",
+        "url": "https://opensource.org/license/mit",
+    },)
 
 Base.metadata.create_all(bind=engine)
 
